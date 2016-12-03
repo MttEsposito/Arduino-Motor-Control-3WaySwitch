ff086aa0071917540177fe1439ed1adba7caca4a
Mattia <MttEsposito@users.noreply.github.com>
2016-12-03 15:26:42 +0100

    Add files via upload

---
+++ Controllo-attuatore-switch.ino
@@ -0,0 +1,39 @@
+/*
+This is a simple project with the Arduino Motor Shield.  
+This code turns the Motor on by a simple 3way switch.
+
+*/
+void setup() {  
+  pinMode(12,OUTPUT); //Channel A Direction Pin Initialize
+  pinMode(6,INPUT); //Input switch1 HIGH
+  pinMode(7,INPUT); //Input switch2 LOW
+  pinMode(5,OUTPUT);  //LED output 1
+  pinMode(4,OUTPUT);  //LED output 2
+}
+
+void loop() {
+    
+
+  // if the switch is high, motor will turn on one direction:
+  if (digitalRead(7) == HIGH)
+  {
+    digitalWrite(12, HIGH); //Channel A Direction Forward
+    analogWrite(3,255); //motor run full speed
+    digitalWrite(5,HIGH); //turn led1 on
+  }
+  
+  // if the switch is low, motor will turn in the other direction:
+  else if(digitalRead(6) == HIGH)
+  {
+    digitalWrite(12, LOW); //Channel A Direction Reverse
+    analogWrite(3,255); //motor run full speed
+    digitalWrite(4,HIGH);//turn led2 on
+  }
+  else
+  //third position switch set all OFF
+  analogWrite(3,0); //motor speed off 
+  digitalWrite(5,LOW);//turn led1 off 
+  digitalWrite(4,LOW);//turn led2 off
+  
+}
+  
